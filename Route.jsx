653d5fa727d0a405801ce65f103b0a15f6a1e1a3
Seanmclem <seanmclements@gmail.com>
2019-11-01 14:37:59 -0400

    initial commit

---
+++ Route.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "solid-js";
+import { RouterContext } from "./SolidjsRouter";
+
+// updating on prop change
+// https://github.com/ryansolid/solid/blob/master/documentation/components.md#components
+
+export const Route = ({ path, component, children }) => {
+    const TheComponent = component;
+    const [state] = useContext(RouterContext);
+
+    return (
+        <Show when={path === state.currentRoute}>
+            <TheComponent />
+        </Show>
+    )
+}
