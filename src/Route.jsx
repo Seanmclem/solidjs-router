cce89c3df1cc94a28d72871ae3a758ff14fc0b30
Seanmclem <seanmclements@gmail.com>
2019-11-10 14:03:12 -0500

    add initial route-param and query-param parsing

--- src/Route.jsx
+++ src/Route.jsx
@@ -4,17 +4,48 @@ import { RouterContext } from "./RouterContext";
 // updating on prop change
 // https://github.com/ryansolid/solid/blob/master/documentation/components.md#components
 
+const getRouteParam = path => {
+    const result = !path || (path && !path.includes(':')) ? '' : path.split(':')[1]
+    return { value: result }
+}
+
+const getQueryParams = path => {
+    if (!path || (path && !path.includes('?'))) {
+        return { value: null };
+    }
+    const paramsStringsArray = path.split('?');
+    const paramsArray = paramsStringsArray[1].split('&');
+    const paramsObjectsArray = paramsArray.map(param => [param.split('=')[0], param.split('=')[1]]);
+    return Object.fromEntries(paramsObjectsArray);
+}
+
 export const Route = ({ path, component, exact }) => {
+
     const [state] = useContext(RouterContext);
     const [showRoute, setShowRoute] = createState({ showPath: false });
+    const [routeParam, setRouteParam] = createState({ value: '' });
+    const [queryParams, setQueryParams] = createState({ value: null });
     const TheComponent = component;
+
     createEffect(() => setShowRoute({
-        showPath: exact ? path === state.currentRoute : state.currentRoute.startsWith(path)
+        showPath: exact ?
+            path === state.currentRoute :
+            state.currentRoute && path.startsWith(state.currentRoute)
     }));
+
+    createEffect(() => {
+        if (showRoute.showPath) {
+            setRouteParam(getRouteParam(path));
+            setQueryParams(getQueryParams(path));
+        }
+    });
 
     return (
         <Show when={showRoute.showPath}>
-            <TheComponent />
+            <TheComponent
+                routeParams={routeParam.value}
+                queryParams={queryParams.value}
+            />
         </Show>
     )
 }
